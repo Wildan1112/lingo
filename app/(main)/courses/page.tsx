a2a660f961fbba1b66fc64d5957f731036834427
Wildan Malul Azam <malul0101@gmail.com>
2024-05-04 09:36:13 +0700

    update

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -6,7 +6,7 @@ const CoursesPage = async () => {
   return (
     <div className="h-full max-w-5xl px-6 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Course</h1>
-      <ListCourses courses={courses} activeCourseId={2} />
+      <ListCourses courses={courses} activeCourseId={1} />
     </div>
   );
 };
