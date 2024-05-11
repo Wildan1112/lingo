ac382cdfd2fe94fe8a338530de835f2d885fd5b9
Wildan Malul Azam <malula0101@gmail.com>
2024-05-11 09:25:27 +0700

    update seed

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,8 +14,75 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values(DATA);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 5, // Spanish
+        title: "Unit 1",
+        description: "Learn the basics of Indonesian",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1, // Unit 1 (Learn the Basics)
+        title: "Nouns",
+        order: 1,
+      },
+      {
+        id: 2,
+        unitId: 1, // Unit 1 (Learn the Basics)
+        title: "Verbs",
+        order: 2,
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1, // Nouns
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "the man" ?',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1, // Which one of these is the "the man"?
+        correct: true,
+        text: "el hombre",
+        imageSrc: "/man.svg",
+        audioSrc: "/sp_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        correct: false,
+        text: "la mujer",
+        imageSrc: "/women.svg",
+        audioSrc: "/sp_women.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        correct: false,
+        text: "el robot",
+        imageSrc: "/robot.svg",
+        audioSrc: "/sp_robot.mp3",
+      },
+    ]);
 
     console.log("Seeding finished");
   } catch (error) {
