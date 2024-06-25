5aa0bd71354c2f5f4d195721f8939f4f248fe086
Wildan Malul Azam <malula0101@gmail.com>
2024-06-25 10:15:04 +0700

    11 challenges card

---
+++ app/lesson/question-bubble.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+
+type Props = {
+  question: string
+}
+
+export const QuestionBubble = ({ question }: Props) => {
+  return (
+    <div className="flex items-center gap-x-6 mb-6">
+      <Image
+        src="/mascot.svg"
+        alt="Mascot"
+        width={60}
+        height={60}
+        className="hidden lg:block"
+      />
+      <Image
+        src="/mascot.svg"
+        alt="Mascot"
+        width={40}
+        height={40}
+        className="lg:hidden block"
+      />
+
+      <div className="relative px-4 py-2 border-2 rounded-xl text-sm lg:text-base">
+
+        {question}
+        <div className="absolute w-0 h-0 -left-3 top-1/2 border-x-8 border-t-8 border-x-transparent transform -translate-y-1/2 rotate-90"></div>
+      </div>
+
+    </div>
+  )
+}
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,30 +1,48 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { challengeOptions, challenges } from "@/db/schema";
 
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
 
 type QuizProps = {
   initialLessonId: number;
   initialHearts: number;
-  initialPercentage: number
+  initialPercentage: number;
   initialLessonChallenges: (typeof challenges.$inferSelect & {
     completed: boolean;
-    challengeOptions: typeof challengeOptions.$inferSelect[];
-  })[]
-  userSubscription: any //TODO: change with type subscription db
-}
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  userSubscription: any; //TODO: change with type subscription db
+};
 
 export const Quiz = ({
   initialLessonId,
   initialHearts,
   initialPercentage,
   initialLessonChallenges,
-  userSubscription
+  userSubscription,
 }: QuizProps) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialHearts);
+
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex !== -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
 
   return (
     <>
@@ -33,6 +51,30 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+
+              <Challenge
+                options={options}
+                onSelect={() => { }}
+                status="none"
+                selectOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
