bfc5ed6b00bcbe77850f9105442d7db1a5c65a49
Wildan Malul Azam <malula0101@gmail.com>
2024-06-28 19:06:12 +0700

    11 Card challenge

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -51,7 +51,6 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
-
       <div className="flex-1">
         <div className="h-full flex items-center justify-center">
           <div className="lg:min-h[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
